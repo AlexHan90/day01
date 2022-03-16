0943c782c1359890ebaeb6228c23be372afa730c
Alex han <s302713@naver.com>
2022-03-16 11:33:18 +0900

    Ex01 print

---
+++ src/day01/Ex01.java
@@ -0,0 +1,22 @@
+package day01; //폴더
+
+public class Ex01 {
+	public static void main(String[] args) { // main > program starting point
+
+		System.out.println("안녕하세요");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		System.out.println("자바시간입니다");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		
+		System.out.print("출력");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		System.out.print("합니다");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		
+										/*
+										  여러줄 주석
+										 */
+		
+		
+											
+		
+		
+	}
+		
+}
