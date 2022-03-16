ce1655503db7ad79f37e03a9be4355cb0df7a5a6
Alex han <s302713@naver.com>
2022-03-16 14:12:36 +0900

    첫날

--- src/day01/Ex01.java
+++ src/day01/Ex01.java
@@ -3,28 +3,32 @@ package day01; //폴더
 public class Ex01 {
 	public static void main(String[] args) { // main > program starting point
 
-		System.out.println("안녕하세요");  // 화면 출력 기능 // "" > 문자열에 대한 기능
-		System.out.println("자바시간입니다");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		//System.out.println("안녕하세요");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		//System.out.println("자바시간입니다");  // 화면 출력 기능 // "" > 문자열에 대한 기능
 		
-		System.out.print("출력");  // 화면 출력 기능 // "" > 문자열에 대한 기능
-		System.out.print("합니다");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		//System.out.print("출력");  // 화면 출력 기능 // "" > 문자열에 대한 기능
+		//System.out.print("합니다"); // 화면 출력 기능 // "" > 문자열에 대한 기능
+		//System.out.print("학원에서 추가");
 		
-										/*
-										  여러줄 주석
+		/*
+		\n : enter 
+		\t : tab 크기만큼 커서를 오른쪽으로 이동
+		\" : "(큰따옴표) 표현
+		\\ : \ 하나 표현								  
+										  
 										 */
-		System.out.print("출력");
-		System.out.print("합니다");
-		System.out.print("학원에서 추가");
+		System.out.println("안\n녕\n하\n세요");
+		System.out.println("1\t123\t12345678\t11");
+		System.out.println(" \"\\ "); // "\
+		System.out.println("\"I\\주가 데이터 분석\"");
 		
 		
 		
 		
+	
 		
-		System.out.println("자바시간입니다");
 		
-		System.out.print("출력");
-		System.out.print("합니다");
-		System.out.println("집에서 추가");
+		
 		
 		
 	
---
+++ src/day01/quiz.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class quiz {
+
+	public static void main(String[] args) {
+		System.out.println("==============================");
+		System.out.println("이름" + "\t" + "나이" + "전화번호" + "회비");
+		System.out.println("==============================");
+		System.out.println("홍길동" + "\t" + " \"" + 15 + "\"" + 3672 + "-" + 1234 + "\\20000");
+		System.out.println("고길동" + "\t" + " \"" + 15 + "\"" + 2238 + "-" + 1234 + "\\30000");
+		System.out.println("김말이" + "\t" + " \"" + 15 + "\"" + 1234 + "-" + 1234 + "\\50000");
+		System.out.println("==============================");
+		System.out.println("총합계" + "\t\t\t" + " \\100000");
+		System.out.println("==============================");
+		 
+		
+	
+
+	}
+
+}
